e3855192093e9626dca9eb336c67984095138eb5
pabloromeroelux <pablo.romero-jaren@electrolux.com>
2022-10-27 08:11:03 +0200

    first version

---
+++ src/code.ts
@@ -0,0 +1,168 @@
+figma.showUI(__html__, { themeColors: true, height: 300 });
+
+function later(delay) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, delay);
+  });
+}
+
+let iconInstances: InstanceNode[] = [];
+let mono;
+
+const checkFigmaSelection = () => {
+  const figmaSelection = figma.currentPage.selection[0];
+  if (
+    figma.currentPage.selection.length === 1 &&
+    figmaSelection &&
+    figmaSelection.type === "INSTANCE" &&
+    figmaSelection.mainComponent.name === "Mono=True"
+  ) {
+    mono = figma.currentPage.selection[0] as InstanceNode;
+    figma.ui.postMessage({
+      type: "enable",
+      value: true,
+    });
+  } else {
+    figma.ui.postMessage({
+      type: "enable",
+      value: false,
+    });
+  }
+};
+figma.on("selectionchange", () => {
+  checkFigmaSelection();
+});
+
+const doInstanceSelection = () => {
+  let items: InstanceNode[] = [];
+  // find icons
+  const instances = figma.currentPage.findAllWithCriteria({
+    types: ["INSTANCE"],
+  });
+
+  items = instances.filter((i) => {
+    try {
+      if (!i.componentProperties) {
+        return false;
+      }
+      return (
+        Object.keys(i.componentProperties).length === 3 &&
+        i.parent.type !== "BOOLEAN_OPERATION" &&
+        i.mainComponent.name !== "Mono=True" &&
+        i.componentProperties.Size &&
+        i.componentProperties.Stroke &&
+        i.componentProperties.Style
+      );
+    } catch (e) {
+      return false;
+    }
+  });
+  return items;
+};
+
+figma.on("run", (event) => {
+  checkFigmaSelection();
+});
+
+figma.ui.onmessage = async (msg) => {
+  let failedIcons: SceneNode[] = [];
+
+  if (msg.type === "scan") {
+    iconInstances = doInstanceSelection();
+    figma.ui.postMessage({
+      type: "selection",
+      value: iconInstances.length,
+    });
+  }
+
+  if (msg.type === "cancel") {
+    figma.closePlugin();
+  }
+
+  if (msg.type === "optimize") {
+    let count = 0;
+    failedIcons = [];
+    for (const instance of iconInstances) {
+      try {
+        // store instance size
+        const width = instance.width;
+        const height = instance.height;
+
+        // store the icon main component
+        const iconComponent = instance.mainComponent;
+
+        // store the icon instance props
+        const iconProps = { ...instance.variantProperties };
+        // store the fills of the icon vector
+        const fills = JSON.parse(
+          JSON.stringify((instance.children[0] as VectorNode).fills)
+        );
+
+        // store vector visibility
+        const vectorVisible = (instance.children[0] as VectorNode).visible;
+
+        const fillStyleId = (instance.children[0] as VectorNode).fillStyleId;
+        const strokeStyleId = (instance.children[0] as VectorNode)
+          .strokeStyleId;
+
+        // swap the icon with an instance of the mono icon.
+        instance.swapComponent(mono.mainComponent);
+        instance.resize(width, height);
+
+        // grab the icon instance within the mono icon.
+        const childrenIcon = (instance.children[0] as BooleanOperationNode)
+          .children[0] as InstanceNode;
+
+        // swap the icon in the new with the initial icon.
+        childrenIcon.swapComponent(iconComponent);
+
+        // add the original fills to the mono icon
+        const union = instance.children[0] as BooleanOperationNode;
+
+        if (strokeStyleId) {
+          union.strokeStyleId = strokeStyleId;
+        } else if (fillStyleId) {
+          union.fillStyleId = fillStyleId;
+        } else {
+          union.fills = fills;
+        }
+
+        if (vectorVisible === false) {
+          union.visible = false;
+        }
+
+        (union.children[0] as InstanceNode).setProperties(iconProps);
+
+        count += 1;
+
+        figma.ui.postMessage({
+          type: "optimized",
+          value: count,
+        });
+      } catch (error) {
+        // do not include the error if the swap was done already
+        if (!error.message.includes("swapComponent")) {
+          failedIcons.push(instance);
+        }
+      }
+      await later(10);
+    }
+
+    iconInstances = doInstanceSelection();
+    figma.ui.postMessage({
+      type: "selection",
+      value: iconInstances.length,
+    });
+    figma.ui.postMessage({
+      type: "failed",
+      value: failedIcons.map((i) => (i as InstanceNode).mainComponent?.name),
+    });
+  }
+
+  if (msg.type === "selectError") {
+    const index = Number(msg.value);
+    const itemFailed = failedIcons[index];
+    figma.currentPage.selection = [itemFailed];
+    figma.viewport.scrollAndZoomIntoView([itemFailed]);
+  }
+};
---
+++ src/ui.tsx
@@ -0,0 +1,128 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./ui.css";
+
+declare function require(path: string): any;
+
+const linkToMono =
+  "https://www.figma.com/file/NHUtkk4ooD27kK4kF8kb9q/UI-Resources?node-id=1%3A450";
+
+function App() {
+  const [selection, setSelection] = React.useState(0);
+  const [optimized, setOptimized] = React.useState(0);
+  const [inProgress, setInProgress] = React.useState(false);
+  const [failed, setFailed] = React.useState([]);
+  const [enabled, setEnabled] = React.useState(false);
+  React.useEffect(() => {
+    onmessage = (event) => {
+      if (event.data.pluginMessage.type === "selection") {
+        setSelection(event.data.pluginMessage.value);
+      }
+      if (event.data.pluginMessage.type === "optimized") {
+        setOptimized(event.data.pluginMessage.value);
+      }
+      if (event.data.pluginMessage.type === "failed") {
+        setFailed(event.data.pluginMessage.value);
+      }
+      if (event.data.pluginMessage.type === "enable") {
+        setEnabled(event.data.pluginMessage.value);
+      }
+    };
+  }, []);
+  React.useEffect(() => {
+    if (inProgress && optimized === selection && selection !== 0) {
+      setInProgress(false);
+    }
+  }, [optimized]);
+
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  const onCreate = () => {
+    setInProgress(true);
+    parent.postMessage({ pluginMessage: { type: "optimize" } }, "*");
+  };
+
+  const onScan = () => {
+    setOptimized(0);
+    parent.postMessage({ pluginMessage: { type: "scan" } }, "*");
+  };
+
+  const onCancel = () => {
+    parent.postMessage({ pluginMessage: { type: "cancel" } }, "*");
+  };
+
+  return (
+    <main>
+      <div className="content">
+        <section className="counter">
+          <h4>
+            <button
+              className="brand"
+              disabled={inProgress}
+              onClick={onScan}
+              style={{ width: "100%" }}
+            >
+              Scan
+            </button>
+          </h4>
+        </section>
+        <section className="counter">
+          <h4>Icons found:</h4> <span>{selection}</span>
+        </section>
+        <section className="counter">
+          <h4>Icons updated:</h4> <span>{optimized}</span>
+        </section>
+
+        {failed.length ? (
+          <>
+            <section className="counter">
+              <h4>Errors:</h4> <span>{failed.length}</span>
+            </section>
+            <ul className="counter">
+              {failed.map((item, i) => (
+                <li key={i}>
+                  <a
+                    className="error"
+                    href="#"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      parent.postMessage(
+                        { pluginMessage: { type: "selectError", value: i } },
+                        "*"
+                      );
+                    }}
+                  >
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          ""
+        )}
+      </div>
+      <footer>
+        {inProgress ? <p>Please wait. Don't touch anything</p> : ""}
+        {enabled ? (
+          ""
+        ) : (
+          <p>
+            You must add and select an instance of{" "}
+            <a href={linkToMono}>Mono Icon</a> in the canvas
+          </p>
+        )}
+        <button
+          className="brand"
+          onClick={onCreate}
+          disabled={selection === 0 || inProgress || !enabled}
+        >
+          Replace
+        </button>
+        <button onClick={onCancel}>Cancel</button>
+      </footer>
+    </main>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("react-page"));
